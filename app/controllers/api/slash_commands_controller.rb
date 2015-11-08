5c59708bc143a77f251a5efab6f2899d6c79aaf3
Nathan Specht <nathanspecht@gmail.com>
2015-11-07 17:35:17 -0800

    return json

--- app/controllers/api/slash_commands_controller.rb
+++ app/controllers/api/slash_commands_controller.rb
@@ -1,6 +1,17 @@
 class Api::SlashCommandsController < ApplicationController
   def create
-    render json: slash_command_params[:user_name]
+    render json: {
+                  "token" => slash_command_params[:token],
+                  "team_id" => slash_command_params[:team_id],
+                  "team_domain" => slash_command_params[:team_domain],
+                  "channel_id" => slash_command_params[:channel_id],
+                  "channel_name" => slash_command_params[:channel_name],
+                  "user_id" => slash_command_params[:user_id],
+                  "user_name" => slash_command_params[:user_name],
+                  "command" => slash_command_params[:command],
+                  "text" => slash_command_params[:text],
+                  "response_url" => slash_command_params[:response_url]
+                 }
   end
 
   private
