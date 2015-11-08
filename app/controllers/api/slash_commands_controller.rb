81a14e65951447eb8ea01ff0d6559cd26ca48853
Nathan Specht <nathanspecht@gmail.com>
2015-11-07 16:43:29 -0800

    Change controller

--- app/controllers/api/slash_commands_controller.rb
+++ app/controllers/api/slash_commands_controller.rb
@@ -1,10 +1,11 @@
 class Api::SlashCommandsController < ApplicationController
   def create
-    render json: {"text": "Received"}
+    render json: {"text" => "Received"}
   end
 
   private
 
   def slash_command_params
+    params.permit(:username)
   end
 end
