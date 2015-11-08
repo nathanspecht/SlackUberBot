f592d81c1cf41b58d3b16fabf63b94f7ec95128d
Nathan Specht <nathanspecht@gmail.com>
2015-11-07 17:23:45 -0800

    testing

--- app/controllers/api/slash_commands_controller.rb
+++ app/controllers/api/slash_commands_controller.rb
@@ -1,10 +1,14 @@
 class Api::SlashCommandsController < ApplicationController
   def create
-    render json: {"text" => request.body.read}
+    render json: slash_command_params
   end
 
   private
 
   def slash_command_params
+    params.permit(:token, :team_id, :team_domain,
+                  :channel_id, :channel_name, :user_id,
+                  :user_name, :command, :text, :response_url)
   end
+
 end
